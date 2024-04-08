f153951f4519dd30362cb614402db5be89726530
akashmanrt <161013742+akashmanrt@users.noreply.github.com>
2024-04-08 16:45:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const captcha = document.getElementById('captcha');
-const ctx = captcha.getContext("2d");
+const captchaQuestion = document.getElementById('captcha-question');
 const userInput = document.getElementById('captcha-input');
 const submitBtn = document.getElementById('submit-btn');
 const errorMessage = document.getElementById('error-message');
 const nextPage = document.getElementById('next-page');
 
-const alphaNums = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
 function generateCaptcha() {
-	let captchaStr = "";
-	for (let i = 1; i <= 7; i++) {
-		captchaStr += alphaNums[Math.floor(Math.random() * alphaNums.length)];
+	const num1 = Math.floor(Math.random() * 10) + 1;
+	const num2 = Math.floor(Math.random() * 10) + 1;
+	const operation = Math.random() > 0.5 ? '+' : '-';
+	let captchaStr = '';
+
+	if (operation === '+') {
+		captchaStr = `${num1} + ${num2} = `;
+	} else {
+		captchaStr = `${num1} - ${num2} = `;
 	}
-	captchaStr = captchaStr.split('').join(' ');
-	ctx.clearRect(0, 0, captcha.width, captcha.height);
-	ctx.font = "30px Roboto";
-	ctx.fillStyle = "#08e5ff";
-	ctx.fillText(captchaStr, captcha.width/4, captcha.height/2);
+
+	captchaQuestion.textContent = captchaStr;
+	captchaQuestion.dataset.answer = operation === '+' ? num1 + num2 : num1 - num2;
 }
 
 function checkCaptcha() {
-	if (userInput.value === captcha.textContent.trim()) {
+	if (parseInt(userInput.value) === parseInt(captchaQuestion.dataset.answer)) {
 		errorMessage.classList.add('hidden');
 		nextPage.classList.remove('hidden');
 	} else {
@@ -28,5 +29,7 @@ function checkCaptcha() {
 		nextPage.classList.add('hidden');
 	}
 }
+
+generateCaptcha();
 
 generateCaptcha();
